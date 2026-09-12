8968c2a292742805fdf806656050534fc19f265c
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2026-09-12 20:58:37 +0900

    feat: [KanuKim97]: 테스트 코드 수정

--- feature/owner/home/src/test/kotlin/com/swyp/mangro/feature/owner/home/OwnerHomeUiStateTest.kt
+++ feature/owner/home/src/test/kotlin/com/swyp/mangro/feature/owner/home/OwnerHomeUiStateTest.kt
@@ -1,7 +1,7 @@
 package com.swyp.mangro.feature.owner.home
 
+import com.swyp.mangro.core.utils.remainingPickupMinutes
 import com.swyp.mangro.feature.owner.home.screen.OwnerHomeUiState
-import com.swyp.mangro.feature.owner.home.screen.utils.remainingPickupMinutes
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
